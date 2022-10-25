f3d4bb8e7c109b0d2aa79c3ce371047175f7dad6
monomadic <monomadic.crypto>
2022-10-25 15:24:49 +0800

    script: generate mnemonic

---
+++ scripts/generate-mnemonic.ts
@@ -0,0 +1,4 @@
+import { Wallet } from "ethers";
+
+const master = Wallet.createRandom()
+console.log(`new mnemonic: \n\taddress:\t${master.address}\n\tprivate key:\t${master.privateKey}\n\tmnemonic:\t${master.mnemonic.phrase}`)
